d6274172178910f1ac8b5b3145c7b85c18f6f20e
Jaco Swarts <jaco@vatfree.com>
2023-04-05 11:24:15 +0200

    Bump Meteor version support

--- package.js
+++ package.js
@@ -1,29 +1,29 @@
 Package.describe({
-  name: 'pauli:accounts-linkedin',
+  name: 'vatfree:accounts-linkedin',
   summary:
-    'Login service for LinkedIn accounts, use with Meteor 1.6.1 & up',
-  version: '5.0.0',
+    'Login service for LinkedIn accounts, use with Meteor 2.3 & up',
+  version: '5.0.1',
   git:
-    'https://github.com/PoBuchi/meteor-accounts-linkedin.git',
+    'https://github.com/vatfree/meteor-accounts-linkedin.git',
 })
 
 Package.onUse(api => {
-  api.versionsFrom('1.6.1')
+  api.versionsFrom('2.3')
   api.use('ecmascript')
   api.use('accounts-base', ['client', 'server'])
   api.imply('accounts-base', ['client', 'server'])
   api.use('accounts-oauth', ['client', 'server'])
-  api.use('pauli:linkedin-oauth@5.0.0', [
+  api.use('vatfree:linkedin-oauth@5.0.1', [
     'client',
     'server',
   ])
-  api.imply('pauli:linkedin-oauth')
+  api.imply('vatfree:linkedin-oauth')
 
   api.use('http', ['client', 'server'])
 
   // If users use accounts-ui but not linkedin-config-ui, give them a tip.
   api.use(
-    ['accounts-ui', 'pauli:linkedin-config-ui@5.0.0'],
+    ['accounts-ui', 'vatfree:linkedin-config-ui@5.0.1'],
     ['client', 'server'],
     { weak: true },
   )
